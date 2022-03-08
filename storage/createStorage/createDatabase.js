fecb0397413f0049e4df195abdf1940c17f22274
abel-parada <abel.parada.millan@gmail.com>
2022-03-08 19:08:47 +0200

    created:statusCodes.json

--- storage/createStorage/createDatabase.js
+++ storage/createStorage/createDatabase.js
@@ -3,7 +3,8 @@
 const Database = require('../database')
 
 const database = require('mime-db');
-let createStatementFile = require('./createStatemens.json')
+let createStatementFile = require('./createStatemens.json');
+const { table } = require('console');
 
 //helper functions for later
 const printMessage = message => console.log(message);
@@ -33,7 +34,7 @@ async function createDb(createStatements){
         password:createStatements.adminpassword
     };
 
-    const DEBUG = createStatements.debug;
+    const DEBUG = createStatements.debug; // why?
     const db = new Database(options);
 
     //for simplicity I allocate the 'user'@'localhost' to a constant
@@ -42,10 +43,57 @@ async function createDb(createStatements){
     //now I allocate the sql commands to constants
     const dropDatabaseSql = `drop database if exists ${createStatements.database}`;
     const createDatabaseSql = `create database ${createStatements.database}`;
-    const dropUserSql = `drop user if exists ${createStatements.user}`;
-    const createUser = `create user ${user} identified by '${createStatements.userpassword}'`;
+    const dropUserSql = `drop user if exists ${user}`;
+    const createUserSql = `create user ${user} identified by '${createStatements.userpassword}'`;
     const grantPrivilegesSql = `grant all privileges on ${createStatements.database}.* to ${user}`;
 
-}
+    try{
+        await db.doQuery(dropDatabaseSql);
+        if(DEBUG) {printStatement(dropDatabaseSql);}
 
-createDb(createStatementFile);
+        await db.doQuery(createDatabaseSql);
+        if(DEBUG){printStatement(createDatabaseSql);}
+
+        await db.doQuery(dropUserSql);
+        if(DEBUG) {printStatement(dropUserSql)}
+
+        await db.doQuery(createUserSql);
+        if(DEBUG) {printStatement(createUserSql)};
+
+        await db.doQuery(grantPrivilegesSql);
+        if(DEBUG){printStatement(grantPrivilegesSql)};
+        //now we create the tables in the createStatemetns.json, in this case 1 (superhero)
+        for(let table of createStatements.tables){
+            if(table.columns && table.columns.length > 0){
+                const createTableSql=
+                `create table ${createStatements.database}.${table.tableName}(`+
+                `\n\t${table.columns.join(',\n\t')}`+
+                ')';
+
+                await db.doQuery(createTableSql);
+                if(DEBUG) printStatement(createTableSql)
+
+                if(table.data && table.data.length>0){
+                    const rows=[];
+                    for(let datum of table.data){
+                        const insertSql = `insert into ${createStatements.database}.${table.tableName} `+`
+                        values(${Array(datum.length).fill('?').join(',')})`;
+                        rows.push(db.doQuery(insertSql,datum))
+                    }
+                    await Promise.all(rows);
+                    if(DEBUG) printMessage('data added');
+                }
+                else {
+                    if(DEBUG) printMessage('data missing');
+                }
+            }
+            else {
+                if (DEBUG) printMessage('Table columns missing. Table was not created');
+            }
+        }
+    }
+    catch(error){
+        printError(error.message);
+    }
+
+}
